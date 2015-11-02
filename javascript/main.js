44e74ca829472eca794178a7c6a76b8bc3075b78
Jef Blocker <jefblocker@gmail.com>
2015-11-01 22:57:11 -0500

    Add JS to display answers on page.  Need to modify styling to prevent overlapping.

--- javascript/main.js
+++ javascript/main.js
@@ -1,6 +1,7 @@
 (function () {
 
-  //Show me how to calculate the average price of all items.
+
+    //Show me how to calculate the average price of all items.
 
   //// Create an array of just the prices
   var prices = items.map( function (item) {
@@ -36,29 +37,41 @@
     // Append newly created node to our answer element
   answer1.appendChild(textNode);
 
-//// 2. Show me how to get an array of items that cost between $14.00 and $18.00 USD
 
 
-// Filter items with price betwen 14.00 and 18.00
-var range = items.filter(function(item) {
-  return item.price > 14.00 && item.price < 18.00;
-  return item.title;
- });
-  console.log(range);
-
-   // Make it a string
-  var str = range;
-  console.log(str);
-
-    // Find our answer element
-  var answer2 = document.querySelector('#answer2');
-    // Create a node from our above answer ready for the DOM
-  var textNode = document.createTextNode(str);
-    // Append newly created node to our answer element
-  answer2.appendChild(textNode);
+// Created a new array for priceFilter variable's items between $14 and $18  
+var arr = [];
 
 
-///// 3. Which item has a "GBP" currency code? Display it's name and price.
+// Return array with items with prices between $14 and $18
+var range = items.filter( function (item) {
+  if (item.price > 14 && item.price < 18) {
+    return arr.push(item.title);
+  };
+});
+
+
+// Return items
+  var answer2a = document.querySelector('#answer2a');
+  var textNode = document.createTextNode(arr[0]);
+  answer2a.appendChild(textNode);
+
+  var answer2b = document.querySelector('#answer2b');
+  var textNode = document.createTextNode(arr[1]);
+  answer2b.appendChild(textNode);
+
+  var answer2c = document.querySelector('#answer2c');
+  var textNode = document.createTextNode(arr[2]);
+  answer2c.appendChild(textNode);
+
+
+
+
+
+
+
+
+  ///// 3. Which item has a "GBP" currency code? Display it's name and price.
 // 1970s Schlitz Malt Liquor Glass Beer Pitcher costs £18
 
 
@@ -85,146 +98,84 @@ var range = items.filter(function(item) {
     // Append newly created node to our answer element
    answer3.appendChild(textNode);
 
-///// 4. Display a list of all items who are made of wood.
-///// SALE Mid Century Siesta Ware White Mug with Anchor - Set of 3 is made of wood.
 
-///// Bottle cap catcher personalized. Man cave gift for him- Wooden Beer pub sign - Groomsmen wedding Gift is made of wood.
 
-///// Medium Size, Welcome To Our Firepit-Where Friends And Marshmallows Get Toasted At The Same Time-Painted Wood Sign-Custom Colors is made of wood.
 
-///// Magnetic Wall Mount Bottle Opener Barware Set - Stainless Steel or Black - Personalized if you like! is made of wood.
 
-///// Engraved Pocket Knife, Personalized Groomsmen Gift, Ring Bearer Gift, Graduation Gift, 4 Knives is made of wood.
 
-// Use forEach function to evaluate items based upon "materials" that mention "wood"
+//Display a list of all items who are made of wood.
+  //Question 4
 
-//var madeOf = items.indexOf(function(item) {
- // item.materials.indexOf("wood") is not -1
-//});
-//console.log(madeOf);
+// Run filter of function to return items containing wood  
+var madeOfWood = items.filter(function (item) {
+    if (item.materials.indexOf('wood') !== -1 )
+      return true;
+  });
+console.log(madeOfWood);
 
-// Determine which items have value of "wood" in the "materials" object array
-//var x = items.filter(function(item) {
-//   if (item.materials === "wood") 
-//   return title;
-//   }); 
-//   console.log(x);
+// Map titles of items made of wood
+var titleOfWood = madeOfWood.map( function (item) {
+    return item.title;
+  });
+  console.log(titleOfWood);
 
-//I need to pull the titles for the the items that have the value of "wood" in the "materials" object array   
-//var y = materials.indexOf("wood") is not -1 {
-//  return title;
-//};
+// Return list of titles of items made of wood
+  var answer4a = document.querySelector('#answer4a');
+  var textNode = document.createTextNode(titleOfWood[0]);
+  answer4a.appendChild(textNode);
+  var answer4b = document.querySelector('#answer4b');
+  var textNode = document.createTextNode(titleOfWood[1]);
+  answer4b.appendChild(textNode);
+  var answer4c = document.querySelector('#answer4c');
+  var textNode = document.createTextNode(titleOfWood[2]);
+  answer4c.appendChild(textNode);
+  var answer4d = document.querySelector('#answer4d');
+  var textNode = document.createTextNode(titleOfWood[3]);
+  answer4d.appendChild(textNode);
+  var answer4e = document.querySelector('#answer4e');
+  var textNode = document.createTextNode(titleOfWood[4]);
+  answer4e.appendChild(textNode);
 
-// Make it a string
-// var str = y + ' is made of wood.'
-// console.log(str);
+//5  Display the name, number of items and the items it is made of.
 
-    // Find our answer element
-//  var answer4 = document.querySelector('#answer4');
-    // Create a node from our above answer ready for the DOM
-//  var textNode = document.createTextNode(str);
-    // Append newly created node to our answer element
-//  answer4.appendChild(textNode);
+var materialsList=[];
 
-///// 5.Which items are made of eight or more materials? Display the name, number of items and the items it is made of.
-
-/////Qty of 2 Groomsmen Gift - Stainless Steel Personalized Bottle Opener - NO Capcatcher has 9 materials:
-// wall mount bottle opener
-// wedding
-// man cave
-// christmas gift
-// guy gift
-// fathers day
-// home bar
-// beer
-// bar
-
-// The Three Broomsticks Customizable Beer Stein Mug, Harry Potter  Inspired, hogsmeade village, harry potter gift, three broomsticks mug  has 13 materials:
-
-// glass
-// sandblast cabinet
-// vinyl
-// beer glass
-// pint glass
-// etched pint glass
-// etched glass
-// etched beer glass
-// 16 oz pint
-// beer gift
-// etched harry potter glass
-// the three broomsticks glass
-// personalized harry potter glass
-
-// Filter items and return items consisting of eight or more materials
-var eightMoreMat = items.filter( function(item) {
-  return item.materials.length >= 8;
+// Show materials for items with more than 8 materials
+var material = items.map(function(item){
+  if (item.materials.length >= 8){
+       return materialsList.push(item);
+  };
 });
+// console.log(listofMaterials);
 
-console.log(eightMoreMat);
-
-// Query document and find element with id #answer5
+// Get information on page
 var answer5 = document.querySelector('#answer5');
 
-// Iterate through items made of eight or more materials
-eightMoreMat.forEach( function(item) {
-  
-  // create p element in #answer5 and add text with title, # of materials, and list of materials.
-  var para = document.createElement('p');
-  para.textContent = item.title + 'has ' + item.materials.length + ' materials';
-  answer5.appendChild(para);
+materialsList.map(function(item){
+ var matList = item.materials.length;
+ var text = " has " + matList + " materials:"
+ var breakLine = document.createElement('br');
+ var title = document.createTextNode(item.title);
+ var textTitle = document.createTextNode(text);
+ var newLine = document.createElement('br');
+ var newLine2 = document.createElement('br');
+ answer5.appendChild(breakLine);
+ answer5.appendChild(title);
+ answer5.appendChild(textTitle);
+ answer5.appendChild(newLine);
 
-  console.log(item.materials.length);
-
-item.materials.forEach( function (itemMaterial) {
-  var textNode = document.createTextNode(itemMaterial);
-
+item.materials.map(function(x){
+ var all = document.createTextNode(x);
+ // var newLine =document.createElement('br');
+ answer5.appendChild(all);
+ // answer5.appendChild(newLine);
+ 
+ });
 });
 
-});
-
-//var range = items.filter(function(item) {
-//  return item.materials.length >= 8;
-//});
-//console.log(range);
-
-//range.forEach(function(item) {
-//  item.quantity;
-//  item.title;
-
-//var div = document.createElement('div');
-//div.textContent = item.title;
-//var body = document.querySelector('body');
-//body.appendChild(div);
-//});
-
-// Filter items with price betwen 14.00 and 18.00
-//var description = range.map(function(item) {
- // return item.title;
- // });
- // console.log(description);
-
-//var quantity = eightPlus.quantity;
-//console.log(quantity);
-
-//var title = eightPlus.title;
-//console.log(title);
-
-//var str = range;
-//console.log(str);
-
-// Make it a string
-//var str = 'Qty of ' + eightPlus.quantity + eightPlus.title + ' has' + eightPlus.materials + ' materials.';
-//console.log(str);
-
-    // Find our answer element
-  //var answer5 = document.querySelector('#answer5');
-    // Create a node from our above answer ready for the DOM
-  //var textNode = document.createTextNode(str);
-    // Append newly created node to our answer element
-  //answer5.appendChild(textNode);
 
 
-///// 6. How many items were made by their sellers?
+  ///// 6. How many items were made by their sellers?
 ///// 18 were made by their sellers
 
 //// Create an array with reduce function to determine how many items were made by their sellers.
@@ -250,7 +201,5 @@ console.log(str);
     // Append newly created node to our answer element
   answer6.appendChild(textNode);
 
+
 }());
-
-
-
